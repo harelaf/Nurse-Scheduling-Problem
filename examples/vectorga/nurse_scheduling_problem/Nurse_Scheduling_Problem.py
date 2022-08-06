ea10156444cd82f828593e3c1a2fde0399a6fb68
Harel Afriat <harelaf@post.bgu.ac.il>
2022-08-06 19:47:30 +0300

    Initial commit, importing files from old repo

---
+++ examples/vectorga/nurse_scheduling_problem/Nurse_Scheduling_Problem.py
@@ -0,0 +1,120 @@
+from SchedulingEvaluator import NurseSchedulingEvaluator
+from SchedulingEvaluator import SHIFTS_PER_WEEK
+from SchedulingEvaluator import SCHEDULING_DAYS
+from SchedulingEvaluator import SHIFTS_PER_DAY
+from SchedulingEvaluator import MORNING_SHIFT
+from SchedulingEvaluator import AFTERNOON_SHIFT
+from SchedulingEvaluator import NIGHT_SHIFT
+from eckity.algorithms.simple_evolution import SimpleEvolution
+from eckity.subpopulation import Subpopulation
+from eckity.creators.ga_creators.bit_string_vector_creator import GABitStringVectorCreator
+from eckity.genetic_operators.crossovers.vector_k_point_crossover import VectorKPointsCrossover
+from eckity.genetic_operators.mutations.vector_random_mutation import BitStringVectorNFlipMutation
+from eckity.genetic_operators.selections.tournament_selection import TournamentSelection
+from eckity.breeders.simple_breeder import SimpleBreeder
+from eckity.statistics.best_average_worst_statistics import BestAverageWorstStatistics
+
+FILENAME = 'Nurses.txt'
+RESULTS_FILE = 'Results.txt'
+NURSE_LIST = []
+CHIEF_LIST = []
+POPULATION_SIZE = 200
+MAX_GENERATION = 500
+
+
+def read_file():
+    with open(FILENAME) as file:
+        lines = file.readlines()
+        for i in range(len(lines)):
+            line = lines[i].strip()
+            if line.startswith('//') or line.startswith(' '):
+                continue
+            elif line.startswith('nurses:'):
+                for j in range(i + 1, len(lines)):
+                    next_line = lines[j].strip()
+                    if len(next_line) == 0 or next_line.startswith('chief nurses:'):
+                        break
+                    NURSE_LIST.append(next_line)
+            elif line.startswith('chief nurses:'):
+                for j in range(i + 1, len(lines)):
+                    next_line = lines[j].strip()
+                    if len(next_line) == 0 or next_line.startswith('nurses:'):
+                        break
+                    CHIEF_LIST.append(next_line)
+            else:
+                continue
+
+
+def print_results(best_of_run):
+    result = ''
+    for day in range(1, SCHEDULING_DAYS + 1):
+        result += 'Day ' + str(day) + '\n'
+        result += '==========\n\n'
+        result += 'Morning Shift - 7AM-2PM\n'
+        result += '==========\n'
+        result += get_shift(best_of_run, day, MORNING_SHIFT)
+        result += '\n'
+        result += 'Afternoon Shift - 2PM-9PM\n'
+        result += '==========\n'
+        result += get_shift(best_of_run, day, AFTERNOON_SHIFT)
+        result += '\n'
+        result += 'Night Shift - 9PM-7AM\n'
+        result += '==========\n'
+        result += get_shift(best_of_run, day, NIGHT_SHIFT)
+        result += '\n'
+    print(result)
+    with open(RESULTS_FILE, 'w+') as file:
+        file.write(result)
+
+
+def get_shift(best_of_run, day, shift):
+    total_nurses = (len(NURSE_LIST) + len(CHIEF_LIST))
+    result = ''
+    result += 'Chief nurses:\n'
+    for nurse in range(len(CHIEF_LIST)):
+        if best_of_run.cell_value(total_nurses * ((day - 1) * SHIFTS_PER_DAY + shift) + nurse):
+            result += CHIEF_LIST[nurse] + '\n'
+    result += '\n'
+    result += 'Nurses:\n'
+    for nurse in range(len(CHIEF_LIST), total_nurses):
+        if best_of_run.cell_value(total_nurses * ((day - 1) * SHIFTS_PER_DAY + shift) + nurse):
+            result += NURSE_LIST[nurse - len(CHIEF_LIST)] + '\n'
+    return result
+
+
+def main():
+    read_file()
+    bit_vector_length = SHIFTS_PER_WEEK * (len(NURSE_LIST) + len(CHIEF_LIST))
+    algo = SimpleEvolution(
+        Subpopulation(
+            # A single bit vector represents the whole schedule, for all nurses.
+            creators=GABitStringVectorCreator(length=bit_vector_length),
+            population_size=POPULATION_SIZE,
+            # For the evaluator, we will use our custom evaluator function.
+            evaluator=NurseSchedulingEvaluator(nurse_amount=len(NURSE_LIST), chief_amount=len(CHIEF_LIST)),
+            # This is a minimization problem, since we want to minimize the total amount of constraints broken.
+            higher_is_better=False,
+            elitism_rate=0.2,
+            # 50% chance for a (SHIFTS_PER_WEEK - 1) point vector-crossover,
+            # 20% chance for an N bit flip mutation with N=0.2*vector_length.
+            operators_sequence=[
+                VectorKPointsCrossover(probability=0.5, k=SHIFTS_PER_WEEK - 1),
+                BitStringVectorNFlipMutation(probability=0.2, n=int(bit_vector_length * 0.2)),
+            ],
+            # Tournament selection with a probability of 1 and with tournament size of 5.
+            selection_methods=[
+                (TournamentSelection(tournament_size=10, higher_is_better=False), 1)
+            ]
+        ),
+        breeder=SimpleBreeder(),
+        max_workers=5,
+        max_generation=MAX_GENERATION,
+        statistics=BestAverageWorstStatistics()
+    )
+    algo.evolve()
+    print(algo.execute())
+    print_results(algo.best_of_run_)
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/vectorga/nurse_scheduling_problem/SchedulingEvaluator.py
@@ -0,0 +1,152 @@
+from eckity.evaluators.simple_individual_evaluator import SimpleIndividualEvaluator
+
+SCHEDULING_DAYS = 7
+SHIFTS_PER_DAY = 3
+SHIFTS_PER_WEEK = SCHEDULING_DAYS * SHIFTS_PER_DAY
+
+MORNING_SHIFT = 0
+AFTERNOON_SHIFT = 1
+NIGHT_SHIFT = 2
+
+MORNING_CHIEF = 1
+MORNING_NURSES = 3
+AFTERNOON_CHIEF = 2
+AFTERNOON_NURSES = 5
+NIGHT_CHIEF = 2
+NIGHT_NURSES = 7
+
+
+class NurseSchedulingEvaluator(SimpleIndividualEvaluator):
+    """
+    Evaluator class for the 'Nurse Scheduling Problem'.
+
+    Attributes
+    -------
+    nurse_amount: int
+        The number of regular nurses.
+    chief_amount: int
+        The number of chief nurses.
+    hard_weight: float
+        The weight of hard constraints.
+    soft_weight: float
+        The weight of soft constraints.
+    total_nurses: int
+        The total amount of nurses (chief and regular).
+    constraints: function list
+        A list of the scheduling constraints, as evaluation functions.
+    individual: any
+        The given schedule given to evaluate its fitness.
+    """
+
+    def __init__(self, nurse_amount, chief_amount, hard_weight=10., soft_weight=1.):
+        super().__init__()
+
+        self.nurse_amount = nurse_amount
+        self.chief_amount = chief_amount
+        self.hard_weight = hard_weight
+        self.soft_weight = soft_weight
+        self.total_nurses = self.nurse_amount + self.chief_amount
+        self.constraints = []
+        self.init_constraints()
+
+    def init_constraints(self):
+        self.constraints.append(self.hard_no_3_consecutive_night_shifts)
+        self.constraints.append(self.hard_no_morning_shift_after_night_shift)
+        self.constraints.append(self.soft_at_least_one_off_day_per_week)
+        self.constraints.append(self.soft_max_3_night_shifts_per_week)
+        self.constraints.append(self.soft_no_3_consecutive_shifts)
+
+    # Hard constraints
+    def hard_no_3_consecutive_night_shifts(self, nurse):
+        consecutive_days = 0
+        for i in range(SCHEDULING_DAYS):
+            block = self.total_nurses * (i * SHIFTS_PER_DAY + NIGHT_SHIFT) + nurse
+            if self.individual.cell_value(block):
+                consecutive_days += 1
+            else:
+                consecutive_days = 0
+            if consecutive_days >= 3:
+                return self.hard_weight
+        if consecutive_days >= 3:
+            return self.hard_weight
+        return 0.
+
+    def hard_no_morning_shift_after_night_shift(self, nurse):
+        for i in range(1, SCHEDULING_DAYS - 1):
+            night_shift = self.total_nurses * ((i - 1) * SHIFTS_PER_DAY + NIGHT_SHIFT) + nurse
+            morning_shift = self.total_nurses * (i * SHIFTS_PER_DAY + MORNING_SHIFT) + nurse
+            if self.individual.cell_value(night_shift) and self.individual.cell_value(morning_shift):
+                return self.hard_weight
+        return 0.
+
+    def hard_shift_nurse_constraints(self, shift, chief_requirement, nurse_requirement):
+        chiefs = 0
+        nurses = 0
+        result = 0.
+        for i in range(SCHEDULING_DAYS):
+            row = (i * SHIFTS_PER_DAY + shift) * self.total_nurses
+            for j in range(self.chief_amount):
+                chiefs += self.individual.cell_value(row + j)
+            for j in range(self.chief_amount, self.total_nurses):
+                nurses += self.individual.cell_value(row + j)
+            if chiefs < chief_requirement:
+                result += self.hard_weight
+            if nurses < nurse_requirement:
+                result += self.hard_weight
+            chiefs = 0
+            nurses = 0
+        return result
+
+    # Soft constraints
+    def soft_at_least_one_off_day_per_week(self, nurse):
+        for i in range(SCHEDULING_DAYS):
+            morning = self.total_nurses * (i * SHIFTS_PER_DAY + MORNING_SHIFT) + nurse
+            afternoon = self.total_nurses * (i * SHIFTS_PER_DAY + AFTERNOON_SHIFT) + nurse
+            night = self.total_nurses * (i * SHIFTS_PER_DAY + NIGHT_SHIFT) + nurse
+            if (not self.individual.cell_value(morning)) and \
+                    (not self.individual.cell_value(afternoon)) and \
+                    (not self.individual.cell_value(night)):
+                return 0.
+        return self.soft_weight
+
+    def soft_max_3_night_shifts_per_week(self, nurse):
+        night_days = 0
+        for i in range(SCHEDULING_DAYS):
+            block = self.total_nurses * (i * SHIFTS_PER_DAY + NIGHT_SHIFT) + nurse
+            if self.individual.cell_value(block):
+                night_days += 1
+            if night_days >= 4:
+                return self.soft_weight
+        if night_days >= 4:
+            return self.soft_weight
+        return 0.
+
+    def soft_no_3_consecutive_shifts(self, nurse):
+        consecutive_shifts = 0
+        for i in range(SHIFTS_PER_WEEK):
+            block = self.total_nurses * i + nurse
+            if self.individual.cell_value(block):
+                consecutive_shifts += 1
+            else:
+                consecutive_shifts = 0
+            if consecutive_shifts >= 3:
+                return self.hard_weight
+        if consecutive_shifts >= 3:
+            return self.hard_weight
+        return 0.
+
+    def _evaluate_individual(self, individual):
+        self.individual = individual
+        evaluation_score = 0.
+
+        # General constraints
+        evaluation_score += self.hard_shift_nurse_constraints(MORNING_SHIFT, MORNING_CHIEF, MORNING_NURSES)
+        evaluation_score += self.hard_shift_nurse_constraints(AFTERNOON_SHIFT, AFTERNOON_CHIEF, AFTERNOON_NURSES)
+        evaluation_score += self.hard_shift_nurse_constraints(NIGHT_SHIFT, NIGHT_CHIEF, NIGHT_NURSES)
+
+        # Constraints list
+        for nurse in range(self.total_nurses):
+            for func in self.constraints:
+                evaluation_score += func(nurse)
+
+        return evaluation_score
